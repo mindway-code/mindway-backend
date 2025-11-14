8aea154d4215f5a963ce2e82cf8aae6f7aef28c0
LucasLydio <mindway.sa@gmail.com>
2025-11-13 22:30:55 -0300

    Editing backend to work with Supabase 10

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,7 +6,7 @@ import Address from '../models/Address.js';
 import Profile from '../models/Profile.js';
 
 import { Op } from 'sequelize';
-import sequelize from '../../config/sequelize';
+import sequelize from '../../config/sequelize.js';
 import * as dotenv from 'dotenv';
 import bcrypt from 'bcryptjs/dist/bcrypt';
 import { v4 as uuidv4 } from 'uuid';
